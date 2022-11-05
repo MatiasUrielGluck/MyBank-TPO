f77d36b58f9ab75f02a0da677b6236ef1e2d63e6
MatiasUrielGluck <matiasugluck@gmail.com>
2022-11-05 14:42:00 -0300

    Agregando metodos

--- apis_tp/Turno.java
+++ apis_tp/Turno.java
@@ -1,5 +1,12 @@
 package apis_tp;
 
 public interface Turno {
-	public void inicializar(String nomenclatura, int id, int tiempoAtencion);
+	public void inicializar(String nomenclatura, int id, int idCola, int tiempoAtencion);
+	
+	public String getNomenclatura();
+	public int getId();
+	public int getColaId();
+	public int getTiempoAtencion();
+	
+	public void setTiempoAtencion(int nuevoTiempo);
 }
--- impl_tp/TurnoTP.java
+++ impl_tp/TurnoTP.java
@@ -10,11 +10,36 @@ public class TurnoTP implements Turno {
 	private int tiempoAtencion;
 	
 	@Override
-	public void inicializar(String nomenclatura, int id, int tiempoAtencion) {
+	public void inicializar(String nomenclatura, int id, int idCola, int tiempoAtencion) {
 		this.nomenclatura = nomenclatura;
 		this.id = id;
+		this.idCola = idCola;
 		this.tiempoAtencion = tiempoAtencion;
-		
+	}
+
+	@Override
+	public String getNomenclatura() {
+		return this.nomenclatura;
+	}
+
+	@Override
+	public int getId() {
+		return this.id;
+	}
+
+	@Override
+	public int getColaId() {
+		return this.idCola;
+	}
+
+	@Override
+	public int getTiempoAtencion() {
+		return this.tiempoAtencion;
+	}
+
+	@Override
+	public void setTiempoAtencion(int nuevoTiempo) {
+		this.tiempoAtencion = nuevoTiempo;		
 	}
 
 }
