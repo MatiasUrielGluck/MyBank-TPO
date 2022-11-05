e9e61508d9d36be36d76dbe65cb061be0d68af2f
MatiasUrielGluck <matiasugluck@gmail.com>
2022-11-05 12:44:19 -0300

    Especificacion del TDA cola

--- apis_tp/Cola.java
+++ apis_tp/Cola.java
@@ -1,5 +1,15 @@
 package apis_tp;
 
 public interface Cola {
-
+	public void inicializar();
+	
+	public void acolarPrioridad(Turno turno);
+	
+	public void desacolar();
+	
+	public Turno primero();
+	
+	public int devolverTiempoTotal();
+	
+	public Boolean colaVacia();
 }
